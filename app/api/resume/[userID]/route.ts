5e786b92c81a6136ccf41ae08813b6b47dad26b0
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-04-06 13:15:44 +0700

    feat: add mongodb, kinde, pdf

---
+++ app/api/resume/[userID]/route.ts
@@ -0,0 +1,49 @@
+const uri = process.env.MONGODB_URI || "";
+import { MongoClient, ServerApiVersion } from "mongodb";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+function getMongoDBClient() {
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  return client;
+}
+export async function GET(request: Request, { params }: { params: { userID: string } }) {
+  const userID = params.userID;
+  let data;
+
+  const client = getMongoDBClient();
+  try {
+    client.connect();
+    const resume = client.db("resume-database").collection("resumes");
+    data = await resume.findOne({ name: userID });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+  if (!data) {
+    return Response.json({ error: "User not found" }, { status: 404 });
+  }
+  return Response.json(data);
+}
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     // write sample data to the database
+//     const resume = client.db("resume-database").collection("resumes");
+//     // const result = await resume.insertOne({ name: "Red", age: 21 });
+//     // console.log(`New listing created with the following id: ${result.insertedId}`);
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,13 +1,76 @@
 "use client";
-import { Page, Text, View, Document, StyleSheet, Image, Font, PDFViewer, Link, render } from "@react-pdf/renderer";
-export default function testPage() {
+
+import { pdf, Page, Text, View, Document, StyleSheet, PDFViewer, Link, BlobProvider } from "@react-pdf/renderer";
+import { useEffect, useMemo, useState } from "react";
+import { Document as PDFViewerDocument, Page as PDFViewerPage } from "react-pdf";
+import { pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = new URL("pdfjs-dist/build/pdf.worker.min.js", import.meta.url).toString();
+
+export default function TestPage() {
+  const [pdfString, setPdfString] = useState("");
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const [doCalcuateBlob, setDoCalculateBlob] = useState(true);
+
+  const onDocumentLoadSuccess = ({ numPages }: any) => {
+    setNumPages(numPages);
+    setPageNumber(1);
+  };
+
+  function changePage(offset) {
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
+  }
+
+  useEffect(() => {
+    const updateBase64String = async () => {
+      const blob = await pdf(Resume()).toBlob();
+      let reader = new FileReader();
+      reader.readAsDataURL(blob);
+      reader.onloadend = () => {
+        const base64String = reader.result as string;
+        setPdfString(base64String);
+      };
+    };
+    if (doCalcuateBlob) updateBase64String();
+  }, [doCalcuateBlob]);
+
   return (
-    <PDFViewer className="w-[96] h-[96] overflow-clip" showToolbar={false}>
-      <Resume />
-    </PDFViewer>
+    <div className="w-screen flex justify-center items-center">
+      <div>
+        <PDFViewerDocument file={pdfString} onLoadSuccess={onDocumentLoadSuccess}>
+          <PDFViewerPage pageNumber={pageNumber} className="border-2 border-black" />
+        </PDFViewerDocument>
+        <div className="border-2 border-black">
+          <p>
+            Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+          </p>
+          <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
+            Previous
+          </button>
+          <button type="button" disabled={pageNumber >= numPages} onClick={nextPage}>
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
-
+{
+  /* <PDFViewer className="w-1/2 h-screen" showToolbar={true}>
+          <Resume />
+        </PDFViewer> */
+}
 const styles = StyleSheet.create({
   body: {
     fontSize: 12,
@@ -99,7 +162,7 @@ const Resume = () => (
       <ResumeSectionHeading>Education</ResumeSectionHeading>
       <ResumeItemHeading title="title" date="22 Aug - Sep 23" subtitle="subtitle" keyword="react, next" />
       {/* flex */}
-      <Description>testing</Description>
+      <Description>asdf</Description>
       <Text style={styles.pageNumber} render={({ pageNumber, totalPages }) => `${pageNumber} / ${totalPages}`} fixed />
     </Page>
   </Document>
