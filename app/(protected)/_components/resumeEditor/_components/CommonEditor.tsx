90621f990db0f98996e71755f1ee00141eb089d6
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-04-30 22:03:47 +0700

    feat: add common editor

---
+++ app/(protected)/_components/resumeEditor/_components/CommonEditor.tsx
@@ -0,0 +1,128 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { resumeDataAtom } from "@/store";
+import { useAtom } from "jotai";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+import editSVG from "@/public/edit.svg";
+import Image from "next/image";
+
+function SVGIcon({ source, className }: { source: any; className: string }) {
+  return (
+    <Image
+      src={source}
+      width={20}
+      height={20}
+      alt="icon"
+      className={className}
+    />
+  );
+}
+export function CommonEditor({
+  id,
+  item,
+  View,
+  Editor,
+}: {
+  id: string;
+  item: any;
+  View: React.ReactNode;
+  Editor: React.ReactNode;
+}) {
+  const [_resumeData, setResumeData] = useAtom(resumeDataAtom);
+
+  const toggleDraft = () => {
+    setResumeData(prev => {
+      const result = prev.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            draft: !item.draft,
+          };
+        }
+        return item;
+      });
+      return result;
+    });
+  };
+
+  const handleDelete = () => {
+    setResumeData(prev => {
+      return prev.filter((item: any) => item.id !== id);
+    });
+  };
+
+  return (
+    <div className={`"mb-2 w-full ${item.draft ? "text-slate-400" : ""}`}>
+      <Dialog>
+        {/* view */}
+        <ContextMenu>
+          <ContextMenuTrigger>
+            <div className="flex justify-between w-full group">
+              {/* right click trigger */}
+              {View}
+              {/* trigger */}
+              <DialogTrigger>
+                <SVGIcon
+                  source={editSVG}
+                  className="hidden group-hover:block"
+                />
+              </DialogTrigger>
+            </div>
+          </ContextMenuTrigger>
+          {/* right click content */}
+          <ContextMenuContent>
+            <DialogTrigger asChild>
+              <ContextMenuItem>edit</ContextMenuItem>
+            </DialogTrigger>
+            <ContextMenuItem onClick={handleDelete}>delete</ContextMenuItem>
+            <ContextMenuItem onClick={toggleDraft}>
+              set as draft
+            </ContextMenuItem>
+          </ContextMenuContent>
+        </ContextMenu>
+        {/* edit */}
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Edit Title</DialogTitle>
+            <DialogDescription>
+              <div className="flex flex-col gap-2">
+                {Editor}
+                {/* common editor */}
+                <div className="flex items-center space-x-2">
+                  <Checkbox
+                    id="draft"
+                    checked={item.draft}
+                    onCheckedChange={toggleDraft}
+                  />
+                  {/* butttons */}
+                  <Label
+                    htmlFor="draft"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    draft
+                  </Label>
+                  {/* delete */}
+                  <Button onClick={handleDelete}>Delete</Button>
+                </div>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
--- app/(protected)/_components/resumeEditor/_components/Title.tsx
+++ app/(protected)/_components/resumeEditor/_components/Title.tsx
@@ -1,30 +1,48 @@
-import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { useState } from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { useEffect, useState } from "react";
+
 import { Label } from "@/components/ui/label";
 import { resumeDataAtom } from "@/store";
 import { useAtom } from "jotai";
-import { Checkbox } from "@/components/ui/checkbox";
-import {
-  ContextMenu,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuTrigger,
-} from "@/components/ui/context-menu";
+import { CommonEditor } from "./CommonEditor";
 
-export default function Title({ data, id }: { data: any; id: string }) {
+function View({ item }: { item: any }) {
+  return (
+    <div>
+      <Label className="text-xs font-semibold text-slate-400">title</Label>
+      <h1 className="text-3xl font-bold">{item.data}</h1>
+    </div>
+  );
+}
+function Editor({
+  item,
+  handleTitleChange,
+}: {
+  item: any;
+  handleTitleChange: any;
+}) {
+  return (
+    <>
+      <Label htmlFor="title">title</Label>
+      <Textarea
+        id="title"
+        value={item.data}
+        onChange={handleTitleChange}
+        placeholder="Title"
+        className="text-2xl"
+      />
+    </>
+  );
+}
+export default function Title({ id }: { id: string }) {
   const [resumeData, setResumeData] = useAtom(resumeDataAtom);
-  const [titleItem, setTitleItem] = useState(
+  const [item, setItem] = useState(
     resumeData.find((item: any) => item.id === id)
   );
+
+  useEffect(() => {
+    setItem(resumeData.find((item: any) => item.id === id));
+  }, [resumeData]);
 
   const handleTitleChange = (e: any) => {
     setResumeData(prev => {
@@ -37,85 +55,16 @@ export default function Title({ data, id }: { data: any; id: string }) {
         }
         return item;
       });
-      setTitleItem(result.find((item: any) => item.id === id));
       return result;
     });
   };
+
   return (
-    <div className="mb-2 w-full">
-      <Dialog>
-        {/* view */}
-        <ContextMenu>
-          <ContextMenuTrigger>
-            <div className="flex justify-between w-full">
-              {/* right click trigger */}
-              <div>
-                <Label className="text-xs font-semibold text-slate-400">
-                  title
-                </Label>
-                <h1 className="text-3xl font-bold">{titleItem.data}</h1>
-              </div>
-              {/* trigger */}
-              <DialogTrigger asChild>
-                <Button>Edit</Button>
-              </DialogTrigger>
-            </div>
-          </ContextMenuTrigger>
-          {/* right click content */}
-          <ContextMenuContent>
-            <DialogTrigger asChild>
-              <ContextMenuItem>edit</ContextMenuItem>
-            </DialogTrigger>
-          </ContextMenuContent>
-        </ContextMenu>
-        {/* edit */}
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Edit Title</DialogTitle>
-            <DialogDescription>
-              <div className="flex flex-col gap-2">
-                <Label htmlFor="title">title</Label>
-                <Textarea
-                  id="title"
-                  value={titleItem.data}
-                  onChange={handleTitleChange}
-                  placeholder="Title"
-                  className="text-2xl"
-                />
-                <div className="flex items-center space-x-2">
-                  <Checkbox
-                    id="draft"
-                    checked={titleItem.draft}
-                    onCheckedChange={_ => {
-                      setResumeData(prev => {
-                        const result = prev.map(item => {
-                          if (item.id === id) {
-                            return {
-                              ...item,
-                              draft: !item.draft,
-                            };
-                          }
-                          return item;
-                        });
-                        setTitleItem(
-                          result.find((item: any) => item.id === id)
-                        );
-                        return result;
-                      });
-                    }}
-                  />
-                  <Label
-                    htmlFor="draft"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    draft
-                  </Label>
-                </div>
-              </div>
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
-    </div>
+    <CommonEditor
+      id={id}
+      item={item}
+      View={View({ item: item })}
+      Editor={Editor({ item: item, handleTitleChange: handleTitleChange })}
+    />
   );
 }
